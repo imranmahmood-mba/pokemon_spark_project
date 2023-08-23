c9b7328cdebbb91eb37a5724062414e07836c36f
imranmahmood-mba <im6979@rit.edu>
2023-08-23 15:18:40 -0700

    changed from methods to attributes

--- bin/pokemon.py
+++ bin/pokemon.py
@@ -25,60 +25,24 @@ class PokemonAPI(IPokemonAPI):
 class Pokemon:
     def __init__(self, pokemon_api: IPokemonAPI, pokemon_name:str=None, id:int=None):
         self.pokemon = pokemon_api.get_pokemon(pokemon_name, id)
-    @property
-    def get_name(self):
-        return self.pokemon['name']
-    @property
-    def get_id(self):
-        return self.pokemon['id']
-    @property
-    def get_base_experience(self):
-        return self.pokemon['base_experience']
-    @property
-    def get_height(self):
-        return self.pokemon['height']
-    @property
-    def get_is_default(self):
-        return self.pokemon('is_default')
-    @property
-    def get_order(self):
-        return self.pokemon['order']
-    @property
-    def get_weight(self):
-        return self.pokemon['weight']
-    @property
-    def get_abilities(self):
-        return self.pokemon['abilities']
-    @property
-    def get_forms(self):
-        return self.pokemon['forms']
-    @property
-    def get_game_indices(self):
-        return self.pokemon['game_indices']
-    @property
-    def get_held_items(self):
-        return self.pokemon['held_items']
-    @property
-    def get_location_area_encounters(self):
-        return self.pokemon['location_area_encounters']
-    @property
-    def get_moves(self):
-        return self.pokemon['moves']
-    @property
-    def get_past_types(self):
-        return self.pokemon['past_types']
-    @property
-    def get_sprites(self):
-        return self.pokemon['sprites']
-    @property
-    def get_species(self):
-        return self.pokemon['species']
-    @property
-    def get_stats(self):
-        return self.pokemon['stats']
-    @property
-    def get_types(self):
-        return self.pokemon['types']
+        self.id = self.pokemon['id']
+        self.name = self.pokemon['name']
+        self.base_experience = self.pokemon['base_experience']
+        self.is_default = self.pokemon['is_default']
+        self.height = self.pokemon['height']
+        self.order = self.pokemon['order']
+        self.weight = self.pokemon['weight']
+        self.abilities = self.pokemon['abilities']
+        self.forms = self.pokemon['forms']
+        self.game_indicies = self.pokemon['game_indices']
+        self.held_items = self.pokemon['held_items']
+        self.location_area_encounters = self.pokemon['location_area_encounters']
+        self.moves = self.pokemon['moves']
+        self.past_types = self.pokemon['past_types']
+        self.sprites = self.pokemon['sprites']
+        self.species = self.pokemon['species']
+        self.stats = self.pokemon['stats']
+        self.types = self.pokemon['types']
 
-char = Pokemon(pokemon_api=PokemonAPI(), pokemon_name='charizard').get_stats
+char = Pokemon(pokemon_api=PokemonAPI(), pokemon_name='charizard').types
 print(char)
