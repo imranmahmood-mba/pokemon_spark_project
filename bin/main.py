665f0bc76361096a82f9048e6987091b531ce2f1
imranmahmood-mba <im6979@rit.edu>
2023-09-03 23:01:02 -0700

    updated dataset calls to new format

--- bin/main.py
+++ bin/main.py
@@ -5,22 +5,24 @@ import pokemon as p
 import pandas as pd
 
 
-def get_pokemon_fact_data():
-    pokemon_data = dp.Dataset(p.PokemonAPI(), 3)
-    weight = pokemon_data.format_weight(pokemon_data.list_of_pokemon_ids)
-    height = pokemon_data.format_height(pokemon_data.list_of_pokemon_ids)
-    types = pokemon_data.format_types(pokemon_data.list_of_pokemon_ids)
-    stats = pokemon_data.format_stats(pokemon_data.list_of_pokemon_ids)
-    abilities = pokemon_data.format_abilities(pokemon_data.list_of_pokemon_ids)
-    games = pokemon_data.format_games(pokemon_data.list_of_pokemon_ids)
-    moves = pokemon_data.format_moves(pokemon_data.list_of_pokemon_ids)
-    name = pokemon_data.format_names(pokemon_data.list_of_pokemon_ids)
-    evolutions = pokemon_data.format_previous_evolutions(pokemon_data.list_of_pokemon_ids)
+def get_pokemon_fact_data(number_of_pokemon:int):
+    pokemon_api = p.PokemonAPI()
+    dataset = dp.Dataset(pokemon_api=pokemon_api, number_of_pokemon=number_of_pokemon)    
+    weight = dataset.pokemon_formatter.format_weight(datasets.list_of_pokemon_ids)
+    height = dataset.pokemon_formatter.format_height(dataset.list_of_pokemon_ids)
+    types = dataset.pokemon_formatter.format_types(dataset.list_of_pokemon_ids)
+    stats = dataset.pokemon_formatter.format_stats(dataset.list_of_pokemon_ids)
+    abilities = dataset.pokemon_formatter.format_abilities(dataset.list_of_pokemon_ids)
+    games = dataset.pokemon_formatter.format_games(dataset.list_of_pokemon_ids)
+    moves = dataset.pokemon_formatter.format_moves(dataset.list_of_pokemon_ids)
+    name = dataset.pokemon_formatter.format_names(dataset.list_of_pokemon_ids)
+    evolutions = dataset.pokemon_formatter.format_previous_evolutions(dataset.list_of_pokemon_ids)
     print(weight, height, types, stats, abilities, games, moves, name)
 
 def game_dimension(number_of_pokemon:int):
-    pokemon_data = dp.Dataset(p.PokemonAPI(), number_of_pokemon=number_of_pokemon)
-    games = pokemon_data.format_games(pokemon_data.list_of_pokemon_ids)
+    pokemon_api = p.PokemonAPI()
+    dataset = dp.Dataset(pokemon_api=pokemon_api, number_of_pokemon=number_of_pokemon)
+    games = dataset.pokemon_formatter.format_games(dataset.list_of_pokemon_ids)
     return games
 
 def pokemon_dimension(number_of_pokemon:int):
